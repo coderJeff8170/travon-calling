9ba22e343f4aadd343ac3b2f50e66a26b3c3c77b
Jeff Andrews <babajeff8170@gmail.com>
2026-05-25 11:36:41 -0400

    corrects email address

--- menu.js
+++ menu.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
       if (!isMobile) {
         e.preventDefault();
         window.location.href =
-          "mailto:Travoncollins217@yahoo.com?subject=Service%20Request&body=Hi%20Travon,%0A%0AI%27d%20like%20to%20request%20a%20service.%0A%0APlease%20reply%20to%20this%20email%20or%20call%20me%20back%20at:%20%3Cplease%20type%20your%20number%20here%3E.%0A%0AThank%20you!";
+          "mailto:Travoncalling225@yahoo.com?subject=Service%20Request&body=Hi%20Travon,%0A%0AI%27d%20like%20to%20request%20a%20service.%0A%0APlease%20reply%20to%20this%20email%20or%20call%20me%20back%20at:%20%3Cplease%20type%20your%20number%20here%3E.%0A%0AThank%20you!";
       }
       // On mobile, the tel: link will handle the dial
     });
